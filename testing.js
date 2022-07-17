e2a30ffce07f9bafcbdcbc8706e931c8cf89355d
quangtran6609 <95970265+quangtran6609@users.noreply.github.com>
2022-07-17 21:35:32 +0700

    add

---
+++ testing.js
@@ -0,0 +1,49 @@
+function getCookie(c_name) {
+    var c_value = document.cookie;
+    var c_start = c_value.indexOf(" " + c_name + "=");
+    if (c_start == -1) {
+        c_start = c_value.indexOf(c_name + "=");
+    }
+    if (c_start == -1) {
+        c_value = null;
+    } else {
+        c_start = c_value.indexOf("=", c_start) + 1;
+        var c_end = c_value.indexOf(";", c_start);
+        if (c_end == -1) {
+            c_end = c_value.length;
+        }
+        c_value = unescape(c_value.substring(c_start, c_end));
+    }
+    return c_value;
+}
+
+function setCookie(c_name, value, exdays) {
+    var exdate = new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
+    document.cookie = c_name + "=" + c_value;
+}
+
+function s4() {
+  return Math.floor((1 + Math.random()) * 0x10000)
+             .toString(16)
+             .substring(1);
+};
+
+function guid() {
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+         s4() + '-' + s4() + s4() + s4();
+}
+
+function checkCookie() {
+    var user = getCookie("user");
+    if (user == null || user == "") {
+        alert("Welcome!!!");
+        user = guid();
+        if (user != null && user != "") {
+            setCookie("user", user, 365);
+        }
+    }
+}
+
+checkCookie();
